576c77d673949ca7528c03406b0e0451bba7af0c
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-07 08:35:18 +0000

    Fixed problem in 2022 Day 7 Part 1

--- Rust/day-07/src/main.rs
+++ Rust/day-07/src/main.rs
@@ -13,22 +13,18 @@ fn cd(i: &str, cur:&mut Vec<String>) {
     };
 }
 
-fn size(from: &str, dirs: &HashMap<String, HashSet<(&str, usize)>>, counted: &mut HashSet<String>) -> usize {
-    let mut s = 0;
-    for &(f, size) in dirs.get(from).unwrap() { 
-        let mut new_s = from.to_owned(); new_s.push_str(f);
-        if !counted.contains(&new_s) {
-            s += size; 
-            counted.insert(new_s);
-        }
-    }
+fn dir_size(from: &str, dirs: &HashMap<String, HashSet<(&str, usize)>>, counted: &mut HashSet<String>) -> usize {
+    let mut s = dirs.get(from).unwrap().iter().filter_map(|&(f, size)| {
+        let full_path = [from, f].join("");
+        let n = (!counted.contains(&full_path)).then(|| size);
+        counted.insert(full_path);
+        n
+    }).sum();
 
-    let ds: Vec<_> = dirs.keys()
+    dirs.keys()
         .filter(|l| l.starts_with(from))
         .filter(|&p| p != from)
-        .collect();
-
-    for d in ds { s += size(d, dirs, counted); }
+        .for_each(|d| s += dir_size(d, dirs, counted));
     s
 }
 
@@ -36,37 +32,34 @@ fn size(from: &str, dirs: &HashMap<String, HashSet<(&str, usize)>>, counted: &mu
 fn navigate(s: &str) -> (usize, usize) {
     let mut dir = vec![];
     let mut dirs: HashMap<String, HashSet<(&str, usize)>> = HashMap::new();
-    dirs.insert("".to_string(), HashSet::new());
 
     for line in s.lines() {
         if line.starts_with("$") {
-            let splits: Vec<_> = line.split_whitespace().take(3).collect();
+            let splits: Vec<_> = line.split_whitespace().collect();
             match splits[1] {
                 "ls" => (),
                 "cd" => cd(splits[2], &mut dir),
                 _ => panic!()
             };
-        } else if !line.starts_with("dir") { // is file
-            let (size, name) =  line.split_once(" ").unwrap();
-            let size = size.parse().unwrap();
-            let current = dir.join("/");
-
-            if !dirs.contains_key(&current) {
-                dirs.insert(current.clone(), HashSet::new());
+        } else {
+            let current_dir = dir.join("/");
+            if !dirs.contains_key(&current_dir) {
+                dirs.insert(current_dir.clone(), HashSet::new());
             }
-            
-            dirs.get_mut(&current).unwrap().insert((name, size));
+
+            if !line.starts_with("dir") {
+                let (fsize, fname) =  line.split_once(' ').unwrap();
+                let fsize = fsize.parse().unwrap();
+                dirs.get_mut(&current_dir).unwrap().insert((fname, fsize));
+            }
         }
     }
     
     let sizes: Vec<_> = dirs.keys()
-    .map(|k| 
-        size(k, &dirs, &mut HashSet::new())
-    ).collect();
+        .map(|k| dir_size(k, &dirs, &mut HashSet::new()) )
+        .collect();
 
-    let free_space = 70_000_000 - sizes.iter().max().unwrap();
-    let delta = 30_000_000  - free_space;
-
+    let delta = 30_000_000 + sizes.iter().max().unwrap() - 70_000_000;
     (sizes.iter().filter(|&&n| n <= 100_000).sum(),
         *sizes.iter().filter(|&&n| n >= delta).min().unwrap())
 }
