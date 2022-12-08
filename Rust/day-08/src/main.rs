6a9d79610b103f1ffc3a8a22aad0c3fab868c894
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-08 07:39:45 +0000

    Further simplified 2022 day 8 solution

--- Rust/day-08/src/main.rs
+++ Rust/day-08/src/main.rs
@@ -1,26 +1,17 @@
-use std::iter;
+use std::iter::repeat;
 
 fn main() {
     let input = include_str!("../../../input/08");
     let rows = process_input(input);
-    let height: usize = rows.len();
-    let width: usize = rows[0].len();
-    
+    let (height, width) = (rows.len(), rows[0].len());
     println!("Part 1: {:?}", p1(&rows, height, width));
     println!("Part 2: {:?}", p2(&rows, height, width));
 }
 
 fn process_input(input: &str) -> Vec<Vec<u8>> {
-    let mut rows = vec![];
-    for line in input.lines() {
-        let row: Vec<u8> = line
-            .trim()
-            .chars()
-            .map(|c| c.to_string().parse().unwrap())
-            .collect();
-        rows.push(row);
-    }
-    rows
+   input.lines().map(|line| {
+        line.trim().chars().map(|c| c.to_digit(10).unwrap() as u8).collect::<Vec<_>>()
+    }).collect()
 }
 
 fn p1(rows: &[Vec<u8>], height: usize, width: usize) -> usize {
@@ -28,14 +19,12 @@ fn p1(rows: &[Vec<u8>], height: usize, width: usize) -> usize {
     for x in 1..(height - 1) {
         for y in 1..(width - 1) {
             fn checker(iter: &mut impl Iterator<Item = (usize, usize)>, x: usize, y: usize, rows: &[Vec<u8>]) -> bool {
-                iter.all(|(r, c)|
-                    if r == x && c == y { true } else { rows[r][c] < rows[x][y] }
-                )
+                iter.all(|(r, c)|  r == x && c == y || rows[r][c] < rows[x][y])
             }
-            if  checker( &mut (0..=x).zip(iter::repeat(y)),            x, y, &rows) || // North
-                checker( &mut (x..=(height - 1)).zip(iter::repeat(y)), x, y, &rows) || // South
-                checker( &mut iter::repeat(x).zip(0..=y),              x, y, &rows) || // West
-                checker( &mut iter::repeat(x).zip(y..=(width - 1)),    x, y, &rows)    // East
+            if checker( &mut (0..=x).zip(repeat(y)), x, y, rows)                // North
+                || checker( &mut (x..=(height - 1)).zip(repeat(y)), x, y, rows) // South
+                || checker( &mut repeat(x).zip(0..=y),              x, y, rows) // West
+                || checker( &mut repeat(x).zip(y..=(width - 1)),    x, y, rows) // East
             {
                 count += 1
             }
@@ -49,16 +38,15 @@ fn p2(rows: &[Vec<u8>], height: usize, width: usize) -> usize {
     for x in 1..(height - 1) {
         for y in 1..(width - 1) {
             fn finder(iter: &mut impl Iterator<Item = (usize, usize)>, x: usize, y: usize, rows: &[Vec<u8>]) -> usize {
-                iter.take_while(|&(r, c)|
-                    if r == x && c == y { true } else { rows[r][c] < rows[x][y] }
-                ).count()
+                iter.take_while(|&(r, c)| r == x && c == y || rows[r][c] < rows[x][y]).count()
             }
             view = view.max(
-                finder( &mut (1..=x).rev().zip(iter::repeat(y)),      x, y, &rows) * 
-                finder( &mut (x..=(height - 2)).zip(iter::repeat(y)), x, y, &rows) * 
-                finder( &mut iter::repeat(x).zip((1..=y).rev()),      x, y, &rows) * 
-                finder( &mut iter::repeat(x).zip(y..=(width - 2)),    x, y, &rows));
-            }
+                  finder( &mut (1..=x).rev().zip(repeat(y)),      x, y, rows)  
+                * finder( &mut (x..=(height - 2)).zip(repeat(y)), x, y, rows)  
+                * finder( &mut repeat(x).zip((1..=y).rev()),      x, y, rows)  
+                * finder( &mut repeat(x).zip(y..=(width - 2)),    x, y, rows)
+            );
+        }
     }
     view 
 }
