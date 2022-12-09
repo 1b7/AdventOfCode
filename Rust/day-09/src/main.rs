89e20e2c6c8cd281b60792f74e085fd36e9b3145
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-09 07:51:54 +0000

    Simplified 2022 Day 9 Solution

--- Rust/day-09/src/main.rs
+++ Rust/day-09/src/main.rs
@@ -13,21 +13,19 @@ fn simulate(input: &str, nk: usize) -> (usize, usize) {
     let mut visited_tail: HashSet<(isize, isize)> = HashSet::new();
     visited_tail.insert(rope[0]);
 
-    let mut visited: HashSet<(isize, isize)> = HashSet::new();
-    visited.insert(rope[0]);
+    let mut visited_first: HashSet<(isize, isize)> = HashSet::new();
+    visited_first.insert(rope[0]);
 
     for line in input.lines() {
         let (dir, n) = line.split_once(' ').unwrap();
         let n: usize = n.parse().unwrap();
 
         for _ in 0..n {
-            // Manually iterating over vec (windows has no mut equivalent)
             for i in 0..(rope.len() - 1) {
                 let mut head = rope[i];
                 let mut tail = rope[i + 1];
-
-                // Only apply instruction to actual head of rope:
-                if i == 0 {
+                
+                if i == 0 { // Only apply instruction to actual head of rope:
                     head = match dir {
                         "R" => (head.0 + 1, head.1),
                         "L" => (head.0 - 1, head.1),
@@ -36,56 +34,18 @@ fn simulate(input: &str, nk: usize) -> (usize, usize) {
                         _ => panic!()
                     };
                 };
-    
-                let dif_y = (head.1 - tail.1).abs();
-                let dif_x = (head.0 - tail.0).abs();
-    
-                if dif_y + dif_x > 2 {
-                    tail = if (head.0 - tail.0) > 1 {
-                        if head.1 > tail.1 {
-                            (tail.0 + 1, tail.1 + 1)
-                        } else {
-                            (tail.0 + 1, tail.1 - 1)
-                        }
-                    } else if (head.1 - tail.1) > 1 {
-                        if head.0 > tail.0 {
-                            (tail.0 + 1, tail.1 + 1)
-                        } else {
-                            (tail.0 - 1, tail.1 + 1)
-                        }
-                    } else if (tail.0 - head.0) > 1 {
-                        if head.1 > tail.1 {
-                            (tail.0 - 1, tail.1 + 1)
-                        } else {
-                            (tail.0 - 1, tail.1 - 1)
-                        }
-                    } else {
-                        if head.0 > tail.0 {
-                            (tail.0 + 1, tail.1 - 1)
-                        } else {
-                            (tail.0 - 1, tail.1 - 1)
-                        }
-                    }
-                } else if dif_x > 1 {
-                    if head.0 > tail.0 {
-                        tail = (head.0 - 1, tail.1)
-                    } else {
-                        tail = (head.0 + 1, tail.1)
-                    }
-                } else if dif_y > 1 {
-                    if head.1 > tail.1 {
-                        tail = (tail.0, head.1 - 1)
-                    } else {
-                        tail = (tail.0, head.1 + 1)
-                    }
+
+                if (head.1 - tail.1).abs() > 1 || (head.0 - tail.0).abs() > 1 {
+                    tail.0 += (head.0 - tail.0).signum();
+                    tail.1 += (head.1 - tail.1).signum();
                 }
 
                 rope[i] = head;
                 rope[i + 1] = tail;
             }
-            visited_tail.insert(rope[1]);
-            visited.insert(rope[nk - 1]);
+            visited_first.insert(rope[1]);
+            visited_tail.insert(rope[nk - 1]);
         }
     }
-    (visited_tail.len(), visited.len())
+    (visited_first.len(), visited_tail.len())
 }
