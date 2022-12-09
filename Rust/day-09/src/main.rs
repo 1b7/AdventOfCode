2832d6fe61d00d0dfe227780ee0b851315b9b4b7
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-09 06:34:53 +0000

    Tided up 2022 Day 9 solution

--- Rust/day-09/src/main.rs
+++ Rust/day-09/src/main.rs
@@ -2,124 +2,31 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../../../input/09");
-    println!("Part 1: {:?}", p1(input));
-    println!("Part 2: {:?}", p2(input));
+    let (p1, p2) = simulate(input, 10);
+    println!("Part 1: {:?}", p1);
+    println!("Part 2: {:?}", p2);
 }
 
-fn p1(input: &str) -> usize {
+fn simulate(input: &str, nk: usize) -> (usize, usize) {
+    let mut rope = vec![(0, 0); nk];
+
+    let mut visited_tail: HashSet<(isize, isize)> = HashSet::new();
+    visited_tail.insert(rope[0]);
+
     let mut visited: HashSet<(isize, isize)> = HashSet::new();
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    visited.insert(tail);
+    visited.insert(rope[0]);
 
     for line in input.lines() {
         let (dir, n) = line.split_once(' ').unwrap();
         let n: usize = n.parse().unwrap();
-        // println!("====== Line: ({}, {}) ======", dir, n);
+
         for _ in 0..n {
-            // println!("PRE - Head: {:?} Tail: {:?}", head, tail);
-            head = match dir {
-                "R" => (head.0 + 1, head.1),
-                "L" => (head.0 - 1, head.1),
-                "U" => (head.0, head.1 + 1),
-                "D" => (head.0, head.1 - 1),
-                _ => panic!()
-            };
+            // Manually iterating over vec (windows has no mut equivalent)
+            for i in 0..(rope.len() - 1) {
+                let mut head = rope[i];
+                let mut tail = rope[i + 1];
 
-
-            let is_adjacent = (tail.0, tail.1) == (head.0, head.1)
-                || (tail.0, tail.1) == (head.0 - 1, head.1)
-                || (tail.0, tail.1) == (head.0 + 1, head.1)
-                || (tail.0, tail.1) == (head.0,     head.1 + 1)
-                || (tail.0, tail.1) == (head.0,     head.1 - 1)
-                || (tail.0, tail.1) == (head.0 + 1, head.1 + 1)
-                || (tail.0, tail.1) == (head.0 - 1, head.1 - 1)
-                || (tail.0, tail.1) == (head.0 - 1, head.1 + 1)
-                || (tail.0, tail.1) == (head.0 + 1, head.1 - 1);
-            
-            let dif_height: isize = head.1 - tail.1;
-            let dif_width: isize = head.0 - tail.0;
-
-            if ((dif_height.abs() > 1 && dif_width.abs() > 0) || (dif_height.abs() > 0 && dif_width.abs() > 1 )) && !is_adjacent {
-                // println!("DIAG CHECK");
-                tail = if (head.0 - tail.0) > 1 {
-                    // println!("DIAG A:");
-                    // (head.0 - 1, tail.1)
-                    if head.1 > tail.1 {
-                        (tail.0 + 1, tail.1 + 1)
-                    } else {
-                        (tail.0 + 1, tail.1 - 1)
-                    }
-                } else if (head.1 - tail.1) > 1 {
-                    // println!("DIAG B:");
-                    // (tail.0, head.1 - 1)
-                    if head.0 > tail.0 {
-                        (tail.0 + 1, tail.1 + 1)
-                    } else {
-                        (tail.0 - 1, tail.1 + 1)
-                    }
-                } else if (tail.0 - head.0) > 1 {
-                    // println!("DIAG C:");
-                    // (tail.0 + 1, head.1)
-                    if head.1 > tail.1 {
-                        (tail.0 - 1, tail.1 + 1)
-                    } else {
-                        (tail.0 - 1, tail.1 - 1)
-                    }
-                } else {
-                    // println!("DIAG D:");
-                    // (tail.0, head.1 + 1)
-                    if head.0 > tail.0 {
-                        (tail.0 + 1, tail.1 - 1)
-                    } else {
-                        (tail.0 - 1, tail.1 - 1)
-                    }
-                }
-            } else if dif_width.abs() > 1 {
-                // println!("MOVE H");
-                if head.0 > tail.0 {
-                    tail = (head.0 - 1, tail.1)
-                } else {
-                    tail = (head.0 + 1, tail.1)
-                }
-            } else if dif_height.abs() > 1 {
-                // println!("MOVE V");
-                if head.1 > tail.1 {
-                    tail = (tail.0, head.1 - 1)
-                } else {
-                    tail = (tail.0, head.1 + 1)
-                }
-            }
-            
-
-            // println!("POST - Head: {:?} Tail: {:?} {}", head, tail, is_adjacent);
-            // if !is_adjacent { panic!("Not Adjacent") }
-            
-            visited.insert(tail);
-        }
-
-    }
-    // println!("{:?}", visited);
-    visited.len()
-}
-
-
-fn p2(input: &str) -> usize {
-    let mut visited: HashSet<(isize, isize)> = HashSet::new();
-    let mut knots = vec![(0, 0); 10];
-    visited.insert(knots[9]);
-
-    for line in input.lines() {
-        let (dir, n) = line.split_once(' ').unwrap();
-        let n: usize = n.parse().unwrap();
-        // println!("====== Line: ({}, {}) ======", dir, n);
-        for _ in 0..n {
-            // println!("{:?}", knots);
-            for i in 0..(knots.len() - 1) {
-                let mut head = knots[i];
-                let mut tail = knots[i + 1];
-
-                // Only move 'real' head:
+                // Only apply instruction to actual head of rope:
                 if i == 0 {
                     head = match dir {
                         "R" => (head.0 + 1, head.1),
@@ -130,22 +37,10 @@ fn p2(input: &str) -> usize {
                     };
                 };
     
+                let dif_y = (head.1 - tail.1).abs();
+                let dif_x = (head.0 - tail.0).abs();
     
-                let is_adjacent = (tail.0, tail.1) == (head.0, head.1)
-                    || (tail.0, tail.1) == (head.0 - 1, head.1)
-                    || (tail.0, tail.1) == (head.0 + 1, head.1)
-                    || (tail.0, tail.1) == (head.0,     head.1 + 1)
-                    || (tail.0, tail.1) == (head.0,     head.1 - 1)
-                    || (tail.0, tail.1) == (head.0 + 1, head.1 + 1)
-                    || (tail.0, tail.1) == (head.0 - 1, head.1 - 1)
-                    || (tail.0, tail.1) == (head.0 - 1, head.1 + 1)
-                    || (tail.0, tail.1) == (head.0 + 1, head.1 - 1);
-                
-                let dif_height: isize = head.1 - tail.1;
-                let dif_width: isize = head.0 - tail.0;
-    
-                if ((dif_height.abs() > 1 && dif_width.abs() > 0) || (dif_height.abs() > 0 && dif_width.abs() > 1 )) && !is_adjacent {
-                    // println!("DIAG CHECK");
+                if dif_y + dif_x > 2 {
                     tail = if (head.0 - tail.0) > 1 {
                         if head.1 > tail.1 {
                             (tail.0 + 1, tail.1 + 1)
@@ -171,24 +66,26 @@ fn p2(input: &str) -> usize {
                             (tail.0 - 1, tail.1 - 1)
                         }
                     }
-                } else if dif_width.abs() > 1 {
+                } else if dif_x > 1 {
                     if head.0 > tail.0 {
                         tail = (head.0 - 1, tail.1)
                     } else {
                         tail = (head.0 + 1, tail.1)
                     }
-                } else if dif_height.abs() > 1 {
+                } else if dif_y > 1 {
                     if head.1 > tail.1 {
                         tail = (tail.0, head.1 - 1)
                     } else {
                         tail = (tail.0, head.1 + 1)
                     }
                 }
-                knots[i] = head;
-                knots[i + 1] = tail;
-                visited.insert(knots[9]);
+
+                rope[i] = head;
+                rope[i + 1] = tail;
             }
+            visited_tail.insert(rope[1]);
+            visited.insert(rope[nk - 1]);
         }
     }
-    visited.len()
+    (visited_tail.len(), visited.len())
 }
