2dabae5beb3d7e5558b488ae69c35d342115991e
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-10 06:54:28 +0000

    Set up for Day 11

---
+++ Rust/day-11/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("../../../input/11");
+    println!("Part 1: {:?}", p1(input));
+    println!("Part 2: {:?}", todo!());
+}
+
+fn p1(input: &str) -> usize {
+    todo!()
+}
