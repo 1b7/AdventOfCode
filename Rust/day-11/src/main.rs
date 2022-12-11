f4721abda5a8d9ce775da5c1b09200920e26646a
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-11 11:17:29 +0000

    Factored out common logic in 2022 Day 11

--- Rust/day-11/src/main.rs
+++ Rust/day-11/src/main.rs
@@ -48,55 +48,38 @@ impl Monkey {
         out
 
     }
-}
+    
+    fn from_str(txt: &str) -> Monkey {
+        let fields: Vec<_> = txt.split('\n').collect();
+        let items: Vec<_> = fields[1].split_once(':').unwrap().1.split(',')
+            .map(|x| x.trim().parse().unwrap())
+            .collect();
 
-fn str_as_monkey(txt: &str) -> Monkey {
-    let fields: Vec<_> = txt.split('\n').collect();
-    let items: Vec<_> = fields[1].split_once(':').unwrap().1.split(',')
-        .map(|x| x.trim().parse().unwrap())
-        .collect();
-    let op = fields[2].split_once("= ").unwrap().1;
-    let ops: Vec<_> = op.split(' ').collect();
-    let (left, mid, right) = (ops[0], ops[1], ops[2]);
-    let left = left.parse().map_or_else(|_| Operand::Old, Operand::Int);
-    let right = right.parse().map_or_else(|_| Operand::Old, Operand::Int);
-    let mid = match mid {
-        "*" => Operator::Mul,
-        _ => Operator::Add
-    };
-    let operation = Operation { left, right, op: mid };
-
-    let test: usize = fields[3].split_once("by ").unwrap().1.parse().unwrap();
-    let test_true: usize = fields[4].split_once("monkey ").unwrap().1.parse().unwrap();
-    let test_false: usize = fields[5].split_once("monkey ").unwrap().1.parse().unwrap();
-
-    Monkey {
-        items, operation, test: (test, test_true, test_false), inspections: 0
+        let ops: Vec<_> = fields[2].split_once("= ").unwrap().1.split(' ').collect();
+        let (left, mid, right) = (ops[0], ops[1], ops[2]);
+        let left = left.parse().map_or_else(|_| Operand::Old, Operand::Int);
+        let right = right.parse().map_or_else(|_| Operand::Old, Operand::Int);
+        let mid = match mid { "*" => Operator::Mul, _ => Operator::Add };
+        let operation = Operation { left, right, op: mid };
+    
+        let test: usize = fields[3].split_once("by ").unwrap().1.parse().unwrap();
+        let test_true: usize = fields[4].split_once("monkey ").unwrap().1.parse().unwrap();
+        let test_false: usize = fields[5].split_once("monkey ").unwrap().1.parse().unwrap();
+    
+        Monkey {
+            items, operation, test: (test, test_true, test_false), inspections: 0
+        }
     }
 }
 
 fn run(input: &str) -> (usize, usize) {
-    let mut monkeys: Vec<_> = input.split("\n\n").map(str_as_monkey).collect();
-    (p1(&mut monkeys.clone()), p2(&mut monkeys))
-}
-
-fn p1(monkeys: &mut [Monkey]) -> usize {
-    for _ in 0..20 {
-        for i in 0..monkeys.len() {
-            let out = monkeys[i].inspect(0);
-            for (x, n) in out {
-                monkeys[n].items.push(x);
-            }
-        }
-    }
-    let mut ins: Vec<usize> = monkeys.iter().map(|m| m.inspections).collect();
-    ins.sort_by(|a, b| b.cmp(a));
-    ins[0..2].iter().product()
-}
-
-fn p2(monkeys: &mut [Monkey]) -> usize {
+    let mut monkeys: Vec<_> = input.split("\n\n").map(Monkey::from_str).collect();
     let p = monkeys.iter().map(|m| m.test.0).product();
-    for _ in 0..10_000 {
+    (px(&mut monkeys.clone(), 20, 0), px(&mut monkeys, 10_000, p))
+}
+
+fn px(monkeys: &mut [Monkey], n: usize, p: usize) -> usize {
+    for _ in 0..n {
         for i in 0..monkeys.len() {
             let out = monkeys[i].inspect(p);
             for (x, n) in out {
