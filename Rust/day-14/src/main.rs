f769422d918ce850cbd209a94e1c3c6514eac6fe
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-14 13:31:17 +0000

    Cleaned up 2022 Day 14 Solution

--- Rust/day-14/src/main.rs
+++ Rust/day-14/src/main.rs
@@ -31,68 +31,39 @@ fn to_set(input: &str) -> HashSet<(usize, usize)> {
             );
 
             // Swapping to avoid empty ranges (10..1 is empty in Rust)
-            if a > x {  (a, x) = swap((a, x)); }
+            if a > x { (a, x) = swap((a, x)); }
             if b > y { (b, y) = swap((b, y)); }
-
             (a..=x).for_each(|m| points.extend((b..=y).map(|n| (m, n))));
         }
     }
     points
 }
 
-fn p1(input: &str) -> usize {
-    let init = to_set(input);
-    let mut points = to_set(input);
+fn p1(input: &str) -> usize { px(input, 1) }
+fn p2(input: &str) -> usize { px(input, 2) }
+fn px(input: &str, part: u8) -> usize {
     let spawn_point = (500, 0);
-    let lowest_y = points.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap().1;
-    let mut has_changed = true;
-
-    while has_changed {
-        let mut tentative = spawn_point;
-        loop {
-            let prior = tentative;
-            tentative.1 += 1;
-            if points.contains(&tentative) {
-                tentative.0 -= 1;
-                if points.contains(&tentative) {
-                    tentative.0 += 2;
-                    if points.contains(&tentative) {
-                        points.insert(prior);
-                        break;
-                    }
-                }
-            }
-            if tentative.1 >= lowest_y {
-                has_changed = false;
-                break;
-            }
-        }
-    }
-    points.difference(&init).count()
-}
-
-fn p2(input: &str) -> usize {
     let mut init = to_set(input);
-    let spawn_point = (500, 0);
+
     let floor_y = init.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap().1 + 2;
     let right_x = init.iter().max_by(|a, b| a.0.cmp(&b.0)).unwrap().0 + 1000;
+    let lowest_y = init.iter().max_by(|a, b| a.1.cmp(&b.1)).unwrap().1;
 
-    for x in 0..=right_x {
-        init.insert((x, floor_y));
-    }
+    init.extend((0..=right_x).map(|x| (x, floor_y)));
     let mut points = init.clone();
-
     let mut has_changed = true;
+    
     while has_changed {
         let mut tentative = spawn_point;
         loop {
-            if points.contains(&spawn_point) {
+            let prior = tentative;
+            tentative.1 += 1;
+
+            if (part == 1 && tentative.1 > lowest_y) || (part == 2 && points.contains(&spawn_point)) {
                 has_changed = false;
                 break;
             }
-            let prior = tentative;
 
-            tentative.1 += 1;
             if points.contains(&tentative) {
                 tentative.0 -= 1;
                 if points.contains(&tentative) {
