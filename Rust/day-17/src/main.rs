bf29b7ef4bb60c8e1b542983f956cde99e00939a
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-17 18:55:13 +0000

    Added Solution to 2022 Day 17 Part 2

--- Rust/day-17/src/main.rs
+++ Rust/day-17/src/main.rs
@@ -1,12 +1,15 @@
 use std::collections::HashSet;
 
 const ROW_WIDTH: usize = 7;
+// Arbitrarily defined constant to circumvent weird issues with repetitions in data:
+const DUP_CHECK: usize = 25; 
 
 fn main() {
     let input = include_str!("../../../input/17");
-    let p1 = p1(input, 2022);
-    // let p1 = p1(input, 1000000000000);
-    println!("Part 1: {}", p1);
+    let s1 = p1(input, 2022);
+    let s2 = p1(input, 1000000000000);
+    println!("Part 1: {}", s1);
+    println!("Part 2: {}", s2);
 
 }
 
@@ -18,13 +21,11 @@ fn shift(c: char, block: &mut Vec<(usize, usize)>, grid: &HashSet<(usize, usize)
             _ => panic!("Unrecognised char '{}'", c)
         }
     }
-
     for &(x, y) in block.iter() {
         if (x == 0 && c == '<') || (x == (ROW_WIDTH - 1) && c == '>') || grid.contains(&(adjust(x, c), y)) {
             return false;
         }
     }
-
     for (x, _) in block.iter_mut() { *x = adjust(*x, c); }
     true
 }
@@ -33,29 +34,8 @@ fn fall(block: &mut Vec<(usize, usize)>, grid: &HashSet<(usize, usize)>, floor: 
     if block.iter().any(|&(x, y)| y <= floor || grid.contains(&(x, y - 1))) {
         return false;
     }
-
     for (_, y) in block.iter_mut() {  *y -= 1; }
     true
-}
-
-fn draw_grid(settled: &HashSet<(usize, usize)>) {
-    let mins = (0, 0);
-    let mut maxes = (0, 0);
-    for &(x, y) in settled {
-        maxes.0 = maxes.0.max(x);
-        maxes.1 = maxes.1.max(y);
-    }
-
-    for y in (mins.1..=maxes.1).rev() {
-        for x in mins.0..=maxes.0 {
-            if settled.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
 }
 
 fn p1(input: &str, total: usize) -> usize {
@@ -69,12 +49,13 @@ fn p1(input: &str, total: usize) -> usize {
 
     let directions: Vec<char> = input.trim().chars().collect();
     let mut ceiling = 0;
-    let mut floor = 0;
+    let mut ceil_off = 0;
     let mut settled = HashSet::new();
-    let mut i = 0;
-
     let mut dir_cursor = 0;
+    let mut states: Vec<(usize, usize, (usize, usize))> = vec![];
 
+    let mut done = false;
+    let mut i = 0;
     while i < total {
         let spawn_point = (2, ceiling + 3);
         let mut new_rock = blocks[i % blocks.len()].clone();
@@ -83,44 +64,42 @@ fn p1(input: &str, total: usize) -> usize {
             *y += spawn_point.1;
         }
 
+        // Simulate falling + wind effects on a per-rock basis:
         loop {
             let direction = directions[dir_cursor % directions.len()];
-
             shift(direction, &mut new_rock, &settled);
             dir_cursor += 1;
+            let fell = fall(&mut new_rock, &settled, 0);
 
-            let fell = fall(&mut new_rock, &settled, floor);
             if !fell {
                 settled.extend(new_rock.iter());
                 let &highest = new_rock.iter().map(|(_, y)| y).max().unwrap();
                 ceiling = ceiling.max(highest + 1);
+
+                let new_state = (i % blocks.len(), dir_cursor % directions.len());
+                let counts = states.iter().filter(|(_, _, pos)| new_state == *pos);
+
+                let mut len = 0;
+                let mut last = None;
+                for x in counts {
+                    last = Some(x);
+                    len += 1;
+                }
+
+                if last.is_some() && len > DUP_CHECK && !done {
+                    done = true;
+                    let &(cyc_ceil, cyc_idx, _) = last.unwrap();
+                    let cycle_height = ceiling - cyc_ceil;
+                    let rem_cycles = (total - i) / (i - cyc_idx);
+
+                    i += (i - cyc_idx) * rem_cycles;
+                    ceil_off = cycle_height * rem_cycles;
+                }
+                states.push((ceiling, i, new_state));
                 break;
             }
         }
-
-        // Trying to reduce space by deleting unreachable elements, i.e. where a
-        // new floor has been created.
-        let affected_rows: HashSet<_> = new_rock.iter().map(|(_, y)| *y).collect(); 
-        let mut new_floors = [None; 5];
-
-        for (i, &row) in affected_rows.iter().enumerate() {
-            for col in 0..ROW_WIDTH {
-                if !settled.contains(&(col, row)) {
-                    new_floors[i] = None;
-                    break;
-                }
-                new_floors[i] = Some(row);
-            }
-        }
-        
-        for nf in new_floors {
-            if let Some(nf) = nf {
-                floor = floor.max(nf);
-                settled.retain(|&(_, y)| y >= nf);
-            }
-        }
-
         i += 1;
     }
-    ceiling
+    ceiling + ceil_off
 }
