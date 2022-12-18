0d2c79744561785b4a89e43cae0dc8e252b3a2f1
1b7 <36208349+1b7@users.noreply.github.com>
2022-12-18 15:48:14 +0000

    Attempted to reduce memory footprint of solution

--- Rust/day-16/src/main.rs
+++ Rust/day-16/src/main.rs
@@ -1,13 +1,17 @@
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::time::Instant;
 
 fn main() {
     let input = include_str!("../../../input/16");
     let graph = process_graph(&get_graph(input));
 
-    let p1 = travel(&graph, 1, 30);
-    // let p2 = travel(&graph, 2, 26);
-    println!("Part 1: {}", p1);
-    // println!("Part 2: {}", p2);
+    let s = Instant::now();
+    // let p1 = travel(&graph, 1, 30);
+    let p2 = travel(&graph, 2, 26);
+    let e = s.elapsed();
+    // println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+    println!("Took: {}", e.as_secs_f32());
 }
 
 fn get_graph(s: &str) -> HashMap<&str, (usize, Vec<&str>)> {
@@ -75,27 +79,25 @@ impl State {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct Traveller {
-    node: String,
-    path: Vec<String>,
+    node: &'static str,
     state: State,
-    opening_valve: Option<String>
+    opening_valve: Option<&'static str>
 }
 
 impl Traveller {
-    pub fn new(node: String, path: Vec<String>, state: State) -> Self { 
-        Traveller { node, path, state, opening_valve: None } 
-    } 
-    // pub fn tick(&mut self) -> State { self.state = self.state.tick(); self.state }
+    pub fn new(node: &'static str, state: State) -> Self { 
+        Traveller { node, state, opening_valve: None } 
+    }
 }
 
-fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: usize) -> usize {
-    let template = Traveller::new("AA".to_string(), vec!["AA".to_string()], State::Free);
+fn travel(graph: &HashMap<&'static str, (usize, Vec<(&'static str, u8)>)>, actors: usize, time: u8) -> usize {
+    let template = Traveller::new("AA", State::Free);
     let travellers = vec![template; actors];
-
-    let mut states: VecDeque<_> = VecDeque::new();
-    states.push_back((travellers, HashSet::<String>::new(), time, 0));
-
+    let all_valves: HashSet<_> = HashSet::from_iter(graph.keys().filter(|&&s| s == "AA").map(|&s| s));
     let mut pressure_max = 0;
+    
+    let mut states: VecDeque<_> = VecDeque::new();
+    states.push_back((travellers, HashSet::<&'static str>::new(), time, 0u16));
 
     // Process a State:
     while !states.is_empty() {
@@ -104,21 +106,30 @@ fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: 
 
         // Turning valves will now be open:
         for actor in &mut travellers {
-            if let Some(valve) = &actor.opening_valve {
-                opened_valves.insert(valve.to_owned());
+            if let Some(valve) = actor.opening_valve {
+                opened_valves.insert(valve);
                 actor.opening_valve = None;
             }
         }
 
         // Include Pressure Lifted:
-        pressure_lifted += opened_valves.iter()
-            .map(|valve| graph.get(valve.as_str()).unwrap().0)
+        let lifted_per_tick = opened_valves.iter()
+            .map(|valve| graph.get(valve).unwrap().0)
             .sum::<usize>();
+        
+        pressure_lifted += lifted_per_tick as u16;
         pressure_max = pressure_max.max(pressure_lifted);
         time_left -= 1;
 
-        let mut outgoing_travellers: Vec<Vec<Traveller>> = vec![vec![]; 2];
+        if opened_valves == all_valves {
+            let remaining =  opened_valves.iter()
+                .map(|valve| graph.get(valve).unwrap().0)
+                .sum::<usize>() * time_left as usize;
+            pressure_max = pressure_max.max(pressure_lifted + remaining as u16);
+            continue;
+        }
 
+        let mut outgoing_travellers: Vec<Vec<Traveller>> = vec![vec![]; 2];
         // 'Act' for each traveller:
         for (n, traveller) in travellers.iter().enumerate() {
             let new_state = traveller.state.tick();
@@ -129,27 +140,25 @@ fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: 
                     outgoing_travellers[n].push(new_traveller);
                 },
                 State::Free => {
-                    let pos = &traveller.node;
+                    let pos = traveller.node;
                     if !opened_valves.contains(pos) && pos != "AA" {
                         let mut new_traveller = traveller.clone();
                         new_traveller.state = State::Moving(1);
-                        new_traveller.opening_valve = Some(pos.to_string());
+                        new_traveller.opening_valve = Some(pos);
                         outgoing_travellers[n].push(new_traveller);
                     } else {
-                        let (_, options) = graph.get(traveller.node.as_str()).unwrap();
+                        let (_, options) = graph.get(traveller.node).unwrap();
 
                         // Only consider travelling to unopened valves:
                         let options = options.into_iter().filter(|(node, dist)| 
-                            !opened_valves.contains(*node)
-                            && *dist as usize <= time_left
+                            !opened_valves.contains(*node) && *dist <= time_left
                         );
 
                         // Create new state, pass it forward:
                         for option in options {
                             let mut new_traveller = traveller.clone();
                             new_traveller.state = State::Moving(option.1);
-                            new_traveller.node = option.0.to_string();
-                            new_traveller.path.push(option.0.to_string());
+                            new_traveller.node = option.0;
                             outgoing_travellers[n].push(new_traveller);
                         }
                     }
@@ -157,17 +166,17 @@ fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: 
             };
         }
 
-        let remaining =  opened_valves.iter()
-            .map(|valve| graph.get(valve.as_str()).unwrap().0)
+        let remaining = opened_valves.iter()
+            .map(|valve| graph.get(valve).unwrap().0)
             .sum::<usize>() * time_left as usize;
-        pressure_max = pressure_max.max(pressure_lifted + remaining);
+        pressure_max = pressure_max.max(pressure_lifted + remaining as u16);
 
         if actors == 2 {
-            // Tes all possible combinations of choices for each actor:
             for t_a in outgoing_travellers[0].iter() {
                 for t_b in outgoing_travellers[1].iter() {
+                    if t_a.opening_valve != None && t_a.opening_valve == t_b.opening_valve { continue; }
                     states.push_back((
-                        vec![t_a.clone()], //, t_b.clone()], 
+                        vec![t_a.clone(), t_b.clone()], 
                         opened_valves.clone(), 
                         time_left, 
                         pressure_lifted
@@ -177,7 +186,7 @@ fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: 
         } else {
             for t_a in outgoing_travellers[0].iter() {
                 states.push_back((
-                    vec![t_a.clone()], //, t_b.clone()], 
+                    vec![t_a.clone()],
                     opened_valves.clone(), 
                     time_left, 
                     pressure_lifted
@@ -185,5 +194,5 @@ fn travel(graph: &HashMap<&str, (usize, Vec<(&str, u8)>)>, actors: usize, time: 
             }
         }
     }
-    pressure_max
+    pressure_max as usize
 }
