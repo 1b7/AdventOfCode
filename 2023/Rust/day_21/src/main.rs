7a3dfb4a62ee7f207e70e931ee5d03c82d96e925
1b7 <36208349+1b7@users.noreply.github.com>
2023-12-21 22:00:23 +0000

    Added code for 2023 Day 21

--- 2023/Rust/day_21/src/main.rs
+++ 2023/Rust/day_21/src/main.rs
@@ -2,9 +2,7 @@ use std::collections::{VecDeque, HashSet};
 
 fn main() {
     let input = include_str!("../../../input/21");
-
     let mut sp = (0, 0);
-
     let grid = input
         .lines()
         .enumerate()
@@ -13,8 +11,15 @@ fn main() {
             chr == '.' || chr == 'S'
         }).collect::<Vec<_>>())
         .collect::<Vec<_>>();
+    
+    println!("Part 1: {}", p1(sp, &grid));
 
-    dbg!(p1(sp, &grid));
+    let (a, b, c) = p2(sp, &grid);
+    println!("Part 2:");
+    println!("  a: 65 + 0(131) iters -> {}", a);
+    println!("  b: 65 + 1(131) iters -> {}", b);
+    println!("  c: 65 + 2(131) iters -> {}", c);
+    println!("\n  Now fit these values to a quadratic function `f`, and calculate `f({})`", 26501365 / grid.len());
 }
 
 
@@ -41,5 +46,43 @@ fn p1(start: (isize, isize), grid: &Vec<Vec<bool>>) -> usize {
         }
         starts = new_starts;
     }
-    dbg!(starts.len())
+    starts.len()
+}
+
+fn p2(start: (isize, isize), grid: &Vec<Vec<bool>>) -> (usize, usize, usize) {
+
+    let (rows, cols) = (grid.len() as isize, grid[0].len() as isize);
+    let orig = (start.0, start.1);
+    let moves = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+    let mut starts = HashSet::new();
+    starts.insert(orig);
+
+    let targets = [
+        (grid.len() - 1) / 2,
+        (grid.len() - 1) / 2 + grid.len(),
+        (grid.len() - 1) / 2 + 2 * grid.len()
+    ];
+    
+    let mut values = (0,0,0);
+
+    for i in 0..=targets[2] {
+        let mut new_starts = HashSet::new();
+        for &start in &starts {
+            for m in moves {
+                let npos = (start.0 + m.0, start.1 + m.1);
+                if npos == start { continue }
+
+                if grid[npos.0.rem_euclid(rows) as usize][npos.1.rem_euclid(cols) as usize] {
+                    new_starts.insert(npos);
+                }
+            }
+        }
+
+        if i == targets[0] { values.0 = starts.len() }
+        if i == targets[1] { values.1 = starts.len() }
+        if i == targets[2] { values.2 = starts.len() }
+        starts = new_starts;
+    }
+    values
 }
