762f704ddd33108eb12a656aaf7352da98831861
1b7 <36208349+1b7@users.noreply.github.com>
2023-12-24 17:03:18 +0000

    Added Solution for 2023 Day 24 Part 2

--- 2023/python/day_24.py
+++ 2023/python/day_24.py
@@ -1,10 +1,11 @@
+import z3
 import numpy as np
 
 def parse_text(s):
   p, v = s.split(" @ ")
   p = [int(x) for x in p.split(", ")]
   v = [int(x) for x in v.split(", ")]
-  return p, v
+  return p + v
 
 def solve_system(ax, ay, ax_c, ay_c, bx, by, bx_c, by_c):
   a = np.array([[ax_c, -bx_c], [ay_c, -by_c]])
@@ -17,17 +18,32 @@ def solve_system(ax, ay, ax_c, ay_c, bx, by, bx_c, by_c):
   except:
     return None
 
-if __name__ == "__main__":
-  with open("input") as fp:
-    input = [parse_text(line.strip()) for line in fp.readlines()]
-
+def p1(input):
   bounds = 200000000000000, 400000000000000
   count = 0
   for i in range(len(input)):
     for j in range(i + 1, len(input)):
-      a = input[i], b = input[j]
-      r = solve_system(a[0][0], a[0][1], a[1][0], a[1][1], b[0][0], b[0][1], b[1][0], b[1][1])
+      a = input[i]
+      b = input[j]
+      r = solve_system(a[0], a[1], a[3], a[4], b[0], b[1], b[3], b[4])
       if r is not None and r[0] >= bounds[0] and r[0] <= bounds[1] and r[1] >= bounds[0] and r[1] <= bounds[1]:
         count += 1
       
-  print("Part 1", count)
+  return count
+
+def p2(input):
+  rock = z3.RealVector('r', 6)
+  time = z3.RealVector('t', 3)
+
+  s = z3.Solver()
+  s.add(*[rock[d] + rock[d+3] * t == hail[d] + hail[d+3] * t
+          for t, hail in zip(time, input) for d in range(3)])
+  s.check()
+
+  return s.model().eval(sum(rock[:3]))
+
+if __name__ == "__main__":
+  with open("input") as fp:
+    input = [parse_text(line.strip()) for line in fp.readlines()]
+    print("Part 1:", p1(input))
+    print("Part 2:", p2(input))
