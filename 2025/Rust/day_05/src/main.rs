58c0ad28108289467ec39b81925b9f75d7b6777c
1b7 <36208349+1b7@users.noreply.github.com>
2025-12-06 23:36:47 +0000

    Added Solution for 2025 Day 05 Part 2

--- 2025/Rust/day_05/src/main.rs
+++ 2025/Rust/day_05/src/main.rs
@@ -1,10 +1,12 @@
+use std::collections::HashSet;
+
 fn main() {
     let (ranges, ingredients) = include_str!("../../../../input/2025/05")
         .split_once("\n\n")
         .expect("Failed to split input");
 
 
-    let ranges = ranges.lines()
+    let mut ranges = ranges.lines()
         .map(|range| {
             let (low, high) = range.split_once('-').expect("Failed to split range");
             (
@@ -19,6 +21,7 @@ fn main() {
         .collect::<Vec<_>>();
 
     println!("Part 1: {}", test_ingredients(&ingredients, &ranges));
+    println!("Part 2: {}", list_fresh_ingredients(&mut ranges));
 }
 
 fn test_ingredients(ingredients: &[usize], ranges: &[(usize, usize)]) -> usize {
@@ -34,4 +37,34 @@ fn test_ingredients(ingredients: &[usize], ranges: &[(usize, usize)]) -> usize {
     }
 
     fresh_ingredients
+}
+
+fn list_fresh_ingredients(ranges: &mut [(usize, usize)]) -> usize {
+    for i in 0..ranges.len() {
+        for j in 0..ranges.len() {
+            if i == j { continue }
+
+            // Subset range
+            if ranges[i].0 <= ranges[j].0  && ranges[i].1 >= ranges[j].1 {
+                ranges[j] = ranges[i];
+                continue;
+            }
+
+            // Overlap on Low Side
+            if ranges[i].0 < ranges[j].0 && ranges[i].1 >= ranges[j].0 {
+                ranges[j].0 = ranges[i].0;
+            }
+
+            // Overlap on High Side
+            if ranges[i].1 > ranges[j].1 && ranges[i].0 <= ranges[j].1 {
+                ranges[j].1 = ranges[i].1;
+            }
+        }
+    }
+
+    ranges.iter()
+        .collect::<HashSet<_>>() // Drop repeat ranges
+        .iter()
+        .map(|&(low, high)| high - low + 1)
+        .sum()
 }
