fedf4acfbe94bbe030370e880221badecdb6342d
1b7 <36208349+1b7@users.noreply.github.com>
2025-12-11 19:49:15 +0000

    Added Solutions for 2025 Day 11.

---
+++ 2025/Rust/day_11/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::{HashMap, VecDeque};
+
+fn parse_line(line: &str) -> (&str, Vec<&str>) {
+    let (src, destinations) = line.split_once(':').unwrap();
+    (src.trim(), destinations.trim().split_whitespace().collect())
+}
+
+fn main() {
+    let graph: HashMap<_, _> = include_str!("../../../../input/2025/11")
+        .lines()
+        .map(parse_line)
+        .collect();
+
+    let p1 = path_to(&graph, "you", "out");
+
+    let p2 = (
+        path_to(&graph, "svr", "fft")
+            * path_to(&graph, "fft", "dac")
+            * path_to(&graph, "dac", "out")
+    ) + (
+        path_to(&graph, "svr", "fft")
+            * path_to(&graph, "dac", "fft")
+            * path_to(&graph, "dac", "out")
+    );
+
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
+}
+
+
+// This is a DAG so we don't need to worry about cycles.
+fn path_to(graph: &HashMap<&str, Vec<&str>>, from: &str, to: &str) -> usize {
+    let mut queue = VecDeque::from([(from, 1)]);
+    let mut unique_paths = 0;
+
+    while let Some((from, mut n)) = queue.pop_front() {
+        queue.retain(|&path| {
+            let retain = path.0 != from;
+            if !retain {  n += path.1; }
+            retain
+        });
+
+        for &option in graph.get(from).unwrap_or(&vec![]) {
+
+            if option == to {
+                unique_paths += n;
+                continue;
+            }
+
+            queue.push_back((option, n));
+        }
+    }
+
+    unique_paths
+}
